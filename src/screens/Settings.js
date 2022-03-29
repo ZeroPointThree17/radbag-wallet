d78dbaa48ccd00934524b809e76bb3b2ea46a2dd
James Velez <zeropointthree17@gmail.com>
2022-03-29 17:17:45 -0500

    updated version numbers

--- src/screens/Settings.js
+++ src/screens/Settings.js
@@ -166,7 +166,7 @@ function onRefresh(refreshCount, setRefreshCount, setRefreshing, setTKUnlock) {
 <View style={{flex: 0.7, justifyContent:"center" , alignSelf:"center"}}>
 <Image style={{margin: 0, padding:0, width: 70, height: 90, alignSelf:'center'}}
     source={Raddish}/>
-    <Text style={[{alignSelf:'center'}, getAppFont("black")]}>RadBag Wallet v2.5.3</Text>
+    <Text style={[{alignSelf:'center'}, getAppFont("black")]}>RadBag Wallet v2.5.4</Text>
 </View>
  <Separator/><Separator/>
  </View>  

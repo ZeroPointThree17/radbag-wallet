a5af501a8eae2cfacd7f579513471a4640f058fe
James Velez <zeropointthree17@gmail.com>
2022-04-01 09:05:54 -0500

    Fixed message decoding for android

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -273,7 +273,7 @@ String.prototype.hexDecode = function(){
   var j;
   var hexes = this.match(/.{1,4}/g) || [];
   var back = "";
-  for(j = 0; j<hexes.length; j++) {
+  for(j = 1; j<hexes.length; j++) {
       back += String.fromCharCode(parseInt(hexes[j], 16));
   }
 

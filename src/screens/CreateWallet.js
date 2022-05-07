0f7ba12f2e5a11055335e55cf2221ff4da871cf3
ZeroPointThree <ZeroPointThree17@gmail.com>
2022-05-07 15:59:00 -0500

    added 18 and 24 seed phrase options

--- src/screens/CreateWallet.js
+++ src/screens/CreateWallet.js
@@ -6,15 +6,16 @@ import CheckboxBouncy from "react-native-bouncy-checkbox";
 import PasswordInputText from 'react-native-password-eye'; 
 import { Separator } from '../helpers/jsxlib';
 import { getAppFont } from '../helpers/helpers';
-// import { ScrollView } from 'react-native-gesture-handler';
-
+import { RadioButton } from 'react-native-paper';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import RadioGroup from 'react-native-radio-buttons-group';
 
 function navigateAppPassword(navigation, mnemonic, word13, firstTime){
 
   var trimmedWord13 = word13.trim();
   var trimmedMnemonic = mnemonic.trim();
   if(word13.length > 0 && trimmedWord13 == ""){
-    alert("13th word cannot consist only of spaces");
+    alert("Custom word cannot consist only of spaces");
   } else {
   const pushAction = StackActions.push('Wallet Password', {
     mnemonicStr: trimmedMnemonic,
@@ -30,50 +31,96 @@ navigation.dispatch(pushAction);
 
 const CreateWallet = ({route, navigation}) => {
 
-  const [mnemonic, setMnemonic] = useState(bip39.generateMnemonic());
+  const [mnemonic, setMnemonic] = useState(bip39.generateMnemonic(128));
   const { firstTimeStr } = route?.params || {};
   var firstTime = JSON.stringify(firstTimeStr).replace(/"/g, '');
 
-const [word13flag, setword13flag] = useState(false);
-const [word13, setword13] = useState("");
+const [wordcustomflag, setwordcustomflag] = useState(false);
+const [wordcustom, setwordcustom] = useState("");
+
 // const [ state, dispatch ] = React.useContext(UserContext)
+
+const radioButtonsData = 
+[
+  {
+  id: '1', // acts as primary key, should be unique and non-empty string
+  label: '12 Words',
+  value: '128',
+  selected: true
+}, {
+  id: '2',
+  label: '18 Words',
+  value: '192',
+  selected: false
+}, {
+  id: '3',
+  label: '24 Words',
+  value: '256',
+  selected: false
+}
+]
+
+const [radioButtons, setRadioButtons] = useState(radioButtonsData)
+
+function onPressRadioButton(radioButtonsArray) {
+  radioButtonsArray.forEach(element => {
+    if(element.selected == true){
+      // alert(element.value)
+      setMnemonic(bip39.generateMnemonic(element.value))
+    }
+  });
+    // alert(JSON.stringify(radioButtonsArray))
+    setRadioButtons(radioButtonsArray);
+}
+
 
   return (
 
-    <ScrollView style={[{backgroundColor:global.reverseModeTranslation}]}> 
-   <Separator/>
-     <View  style={[styles.container,{backgroundColor:global.reverseModeTranslation}]}> 
+  <ScrollView style={[{backgroundColor:global.reverseModeTranslation}]}> 
+  <Separator/>
+  <View  style={[styles.container,{backgroundColor:global.reverseModeTranslation}]}> 
+
       <Text style={[styles.title,getAppFont("black")]}>Below is your mnemonic phrase. Write it down and keep them in a safe place. DO NOT take a photo of the phrase or copy and paste it anywhere. This key holds ALL YOUR FUNDS!</Text>
- <Separator/>
- <Text style={[styles.mnemonic,getAppFont("black")]}>{mnemonic}</Text>
- <Separator/>
+      <Separator/>
+      <Text style={[styles.title,getAppFont("black")]}>Select the number of words you want the mnemonic to be:</Text>
  
- <View style={styles.checkbox}> 
+      <RadioGroup layout = "row"
+                  radioButtons={radioButtons} 
+                  onPress={onPressRadioButton} 
+              />
+      <Separator/>
+      <Separator/>
+     <Text style={[styles.title,getAppFont("black")]}>Mnemonic Phrase:</Text>
+ 
+      <Text style={[styles.mnemonic,getAppFont("black")]}>{mnemonic}</Text>
+      <Separator/>
+ 
+    <View style={styles.checkbox}> 
 
-<CheckboxBouncy
-fillColor="#183A81"
-iconStyle={{ borderColor: global.modeTranslation }}
-isChecked={word13flag}
-onPress={()=>{
-  if(word13flag==false){
-  setword13flag(true);
-  }
-  else setword13flag(false);
-}}
-/>
+          <CheckboxBouncy
+          fillColor="#183A81"
+          iconStyle={{ borderColor: global.modeTranslation }}
+          isChecked={wordcustomflag}
+          onPress={()=>{
+            if(wordcustomflag==false){
+            setwordcustomflag(true);
+            }
+            else setwordcustomflag(false);
+          }}
+          />
 
-<Text style={[styles.title2,getAppFont("black")]}>Add 13th word?</Text>
-</View > 
-{ word13flag && <React.Fragment><View style={{width: "80%"}}><PasswordInputText placeholder="13th word" secureTextEntry={true} eyeColor={global.modeTranslation} placeholderTextColor={global.modeTranslation} underlineColor={global.modeTranslation}  inputStyle={[{ padding:4, paddingLeft:10, height:40, borderRadius: 15, borderWidth: 1, borderColor: global.modeTranslation, color:global.modeTranslation}, getAppFont("black")]} 
-onChangeText={(password) => setword13( password )}/></View></React.Fragment>
- }
-<Separator/>
-<Separator/>
- <Button style={[styles.title, getAppFont("black")]}
-        title="Understood - Continue"
-        enabled
-        onPress={() => navigateAppPassword(navigation, mnemonic, word13, firstTime)}
-      />
+        <Text style={[styles.title2,getAppFont("black")]}>Add custom word?</Text>
+    </View > 
+    { wordcustomflag && <React.Fragment><View style={{width: "80%"}}><PasswordInputText placeholder="Custom Word" secureTextEntry={true} eyeColor={global.modeTranslation} placeholderTextColor={global.modeTranslation} underlineColor={global.modeTranslation}  inputStyle={[{ padding:4, paddingLeft:10, height:40, borderRadius: 15, borderWidth: 1, borderColor: global.modeTranslation, color:global.modeTranslation}, getAppFont("black")]} 
+    onChangeText={(password) => setwordcustom( password )}/></View></React.Fragment>
+    }
+    <Separator/>
+    <Separator/>
+    <Button style={[styles.title, getAppFont("black")]}
+            title="Understood - Continue"
+            enabled
+            onPress={() => navigateAppPassword(navigation, mnemonic, wordcustom, firstTime)}
+          />
   </View> 
   <Separator/>
   <Separator/>
